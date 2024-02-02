70c429a0f3b7ad67f9d2d540912661f5be884efc
Daehee Mok <100769596+DaeHee99@users.noreply.github.com>
2024-02-03 04:16:47 +0900

    [level 2] Title: 가장 큰 수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -0,0 +1,7 @@
+function solution(numbers) {
+    const answer = numbers.sort((a, b) => {
+        return `${b}${a}` - `${a}${b}`;
+    }).join("");
+    
+    return answer[0] === "0" ? "0" : answer;
+}
