96346a9393019e3a3f3f7c0d2f5d48df69c09b96
Daehee Mok <100769596+DaeHee99@users.noreply.github.com>
2024-12-19 02:58:58 +0900

    [Bronze IV] Title: $A + B - C$, Time: 88 ms, Memory: 9580 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const [a, b, c] = input.map(Number);
+
+console.log(a + b - c);
+console.log(`${a}${b}` - c);
