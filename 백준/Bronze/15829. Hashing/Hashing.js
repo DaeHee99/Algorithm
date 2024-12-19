b793a6403c7c241aad818ead0dc264e976f02296
Daehee Mok <100769596+DaeHee99@users.noreply.github.com>
2024-12-20 00:19:50 +0900

    [Bronze II] Title: Hashing, Time: 100 ms, Memory: 9584 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.js
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -2,11 +2,15 @@ const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 const [l, s] = input;
+const M = 1234567891;
+const R = 31;
 let answer = 0;
+let rPower = 1;
 
 for (let i = 0; i < Number(l); i++) {
   const a = s[i].charCodeAt() - "a".charCodeAt() + 1;
-  answer += a * 31 ** i;
+  answer = (answer + ((a * rPower) % M)) % M;
+  rPower = (rPower * R) % M;
 }
 
-console.log(answer % 1234567891);
+console.log(answer);
